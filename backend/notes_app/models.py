19e5eb934987157172b38be9696a3ab68fc0156e
RefiyeK <205158404+RefiyeK@users.noreply.github.com>
2026-05-04 20:57:23 +0200

    feat: setup DRF backend with Note model, serializer, and viewset

---
+++ backend/notes_app/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Note(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    marked = models.BooleanField(default=False)
+    trash = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.title
